8d1fa8fc21fa9b5854fbbd93fae47a6ba36fe6d8
sam <samaniloqman91@gmail.com>
2024-03-08 14:11:31 +0100

    biostoch update

---
+++ package/biostoch/__main__.py
@@ -0,0 +1,10 @@
+def main():
+    print("Welcome to the biostoch package!")
+    print("This package provides stochastic and deterministic simulation methods used in computational biology.")
+    print("To get started, you can run specific examples or import the package modules for your own use.")
+    print("for more information visit https://github.com/LoqmanSamani/biostoch")
+
+
+if __name__ == "__main__":
+    main()
+
